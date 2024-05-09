26a346bc8c2d37f189a0e8d45dc1be86f64e26be
Enzo Vasconcelos <enzo.vasconcelos@ccc.ufcg.edu.br>
2024-05-09 20:56:20 -0300

    Fix: corrigindo escolha do escalonador no Main

--- Main.java
+++ Main.java
@@ -6,38 +6,21 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        Escalonador escalonador;
-        String alg = args[0];
-        if(alg == "FCFS")
-            escalonador = new FCFS();
-        else
-            escalonador = new SSTF();
-    
-    
         final double AVG_SEEK = 9;
         final int RPMS = 7200;
         Disco disco = new Disco(AVG_SEEK, RPMS);
-        disco.setEscalonador(escalonador);
+        disco.setEscalonador(new FCFS());
     
         final int NUM_BLOCOS = 80;
         int[] blocos = new int[NUM_BLOCOS];
         blocos = new int[] {
             78, 40, 67, 47, 37, 27, 75, 31, 0, 33, 56, 32, 24, 43, 76, 53, 76, 45, 12, 58, 45, 14, 60, 71, 22, 40, 73, 14, 57, 29, 32, 71, 35, 6, 76, 68, 18, 25, 45, 67, 13, 55, 35, 49, 65, 46, 9, 69, 61, 34, 79, 48, 51, 35, 32, 63, 47, 11, 3, 10, 67, 38, 76, 77, 33, 14, 50, 0, 2, 3, 12, 20, 51, 51, 4, 77, 15, 11, 61, 7
         };
-        // Random randomGen = new Random();
-        // for (int i = 0; i < blocos.length; i++) {
-            // blocos[i] = randomGen.nextInt(NUM_BLOCOS);
-        // }   
 
-        System.out.println("Blocos buffer: " + Arrays.toString(blocos));
+        System.out.println("Lista de blocos: " + Arrays.toString(blocos));
 
-        // disco.setEscalonador(new SSTF());
         disco.write(blocos);
-        System.out.println("Tempo total (ms): " + disco.write() + " - " + alg);
-
-//        disco.setEscalonador(new FCFS());
- //       disco.write(blocos);
-  //      System.out.println("Tempo total (ms) SSTF: " + disco.write());
+        System.out.println("Tempo total (ms): " + disco.write());
     }
     
 }
